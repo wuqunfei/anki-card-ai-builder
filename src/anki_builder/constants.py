bd966725033ce59df5544928d3aebf67fb84b77a
Qunfei Wu <wu.qunfei@gmail.com>
2026-04-24 03:02:00 +0200

    refactor: add shared constants module

---
+++ src/anki_builder/constants.py
@@ -0,0 +1,6 @@
+# src/anki_builder/constants.py
+MINIMAX_BASE_URL = "https://api.minimax.io/anthropic"
+MINIMAX_MODEL = "MiniMax-M2.5"
+MINIMAX_IMAGE_URL = "https://api.minimax.io/v1/image_generation"
+MINIMAX_IMAGE_MODEL = "image-01"
+MAX_RETRIES = 3
